4b006419c296077579f70917b901642f6fe172c8
faizanzafar97 <faizanzafar8672@gmail.com>
2026-09-17 15:36:35 +0500

    optimize version

--- cards-project/src/App.jsx
+++ cards-project/src/App.jsx
@@ -1,137 +1,117 @@
-import React from 'react'
-import Card from './components/Card'
+import React from "react";
+import Card from "./components/Card";
+
 const App = () => {
-
-const jobs = [
-  {
-    brandLogo: "https://www.google.com/s2/favicons?domain=google.com&sz=128",
-    name: "Google",
-    datePosted: "5 days ago",
-    post: "UI/UX Designer",
-    tag1: "Part-Time",
-    tag2: "Senior Level",
-    pay: "$120/hr",
-    location: "Islamabad, Pakistan"
-  },
-
-  {
-    brandLogo: "https://www.google.com/s2/favicons?domain=microsoft.com&sz=128",
-    name: "Microsoft",
-    datePosted: "2 days ago",
-    post: "Frontend Developer",
-    tag1: "Full-Time",
-    tag2: "Junior Level",
-    pay: "$100/hr",
-    location: "Islamabad, Pakistan"
-  },
-
-  {
-    brandLogo: "https://www.google.com/s2/favicons?domain=amazon.com&sz=128",
-    name: "Amazon",
-    datePosted: "1 week ago",
-    post: "Senior UI/UX Designer",
-    tag1: "Part-Time",
-    tag2: "Senior Level",
-    pay: "$120/hr",
-    location: "Islamabad, Pakistan"
-  },
-
-  {
-    brandLogo: "https://www.google.com/s2/favicons?domain=meta.com&sz=128",
-    name: "Meta",
-    datePosted: "3 days ago",
-    post: "React Developer",
-    tag1: "Full-Time",
-    tag2: "Mid Level",
-    pay: "$110/hr",
-    location: "Islamabad, Pakistan"
-  },
-
-  {
-    brandLogo: "https://www.google.com/s2/favicons?domain=apple.com&sz=128",
-    name: "Apple",
-    datePosted: "10 weeks ago",
-    post: "Product Designer",
-    tag1: "Full-Time",
-    tag2: "Senior Level",
-    pay: "$150/hr",
-    location: "Islamabad, Pakistan"
-  },
-
-  {
-    brandLogo: "https://www.google.com/s2/favicons?domain=netflix.com&sz=128",
-    name: "Netflix",
-    datePosted: "4 days ago",
-    post: "Backend Developer",
-    tag1: "Full-Time",
-    tag2: "Senior Level",
-    pay: "$140/hr",
-    location: "Islamabad, Pakistan"
-  },
-
-  {
-    brandLogo: "https://www.google.com/s2/favicons?domain=tesla.com&sz=128",
-    name: "Tesla",
-    datePosted: "1 week ago",
-    post: "Software Engineer",
-    tag1: "Full-Time",
-    tag2: "Mid Level",
-    pay: "$130/hr",
-    location: "Islamabad, Pakistan"
-  },
-
-  {
-    brandLogo: "https://www.google.com/s2/favicons?domain=adobe.com&sz=128",
-    name: "Adobe",
-    datePosted: "6 days ago",
-    post: "Graphic Designer",
-    tag1: "Part-Time",
-    tag2: "Junior Level",
-    pay: "$90/hr",
-    location: "Islamabad, Pakistan"
-  },
-
-  {
-    brandLogo: "https://www.google.com/s2/favicons?domain=ibm.com&sz=128",
-    name: "IBM",
-    datePosted: "2 weeks ago",
-    post: "Data Analyst",
-    tag1: "Full-Time",
-    tag2: "Mid Level",
-    pay: "$105/hr",
-    location: "Islamabad, Pakistan"
-  },
-
-  {
-    brandLogo: "https://www.google.com/s2/favicons?domain=oracle.com&sz=128",
-    name: "Oracle",
-    datePosted: "3 days ago",
-    post: "Cloud Engineer",
-    tag1: "Full-Time",
-    tag2: "Senior Level",
-    pay: "$135/hr",
-    location: "Islamabad, Pakistan"
-  }
-];
+  const jobs = [
+    {
+      brandLogo: "https://www.google.com/s2/favicons?domain=google.com&sz=128",
+      name: "Google",
+      datePosted: "5 days ago",
+      post: "UI/UX Designer",
+      tag1: "Part-Time",
+      tag2: "Senior Level",
+      pay: "$120/hr",
+      location: "Islamabad, Pakistan",
+    },
+    {
+      brandLogo: "https://www.google.com/s2/favicons?domain=microsoft.com&sz=128",
+      name: "Microsoft",
+      datePosted: "2 days ago",
+      post: "Frontend Developer",
+      tag1: "Full-Time",
+      tag2: "Junior Level",
+      pay: "$100/hr",
+      location: "Islamabad, Pakistan",
+    },
+    {
+      brandLogo: "https://www.google.com/s2/favicons?domain=amazon.com&sz=128",
+      name: "Amazon",
+      datePosted: "1 week ago",
+      post: "Senior UI/UX Designer",
+      tag1: "Part-Time",
+      tag2: "Senior Level",
+      pay: "$120/hr",
+      location: "Islamabad, Pakistan",
+    },
+    {
+      brandLogo: "https://www.google.com/s2/favicons?domain=meta.com&sz=128",
+      name: "Meta",
+      datePosted: "3 days ago",
+      post: "React Developer",
+      tag1: "Full-Time",
+      tag2: "Mid Level",
+      pay: "$110/hr",
+      location: "Islamabad, Pakistan",
+    },
+    {
+      brandLogo: "https://www.google.com/s2/favicons?domain=apple.com&sz=128",
+      name: "Apple",
+      datePosted: "10 weeks ago",
+      post: "Product Designer",
+      tag1: "Full-Time",
+      tag2: "Senior Level",
+      pay: "$150/hr",
+      location: "Islamabad, Pakistan",
+    },
+    {
+      brandLogo: "https://www.google.com/s2/favicons?domain=netflix.com&sz=128",
+      name: "Netflix",
+      datePosted: "4 days ago",
+      post: "Backend Developer",
+      tag1: "Full-Time",
+      tag2: "Senior Level",
+      pay: "$140/hr",
+      location: "Islamabad, Pakistan",
+    },
+    {
+      brandLogo: "https://www.google.com/s2/favicons?domain=tesla.com&sz=128",
+      name: "Tesla",
+      datePosted: "1 week ago",
+      post: "Software Engineer",
+      tag1: "Full-Time",
+      tag2: "Mid Level",
+      pay: "$130/hr",
+      location: "Islamabad, Pakistan",
+    },
+    {
+      brandLogo: "https://www.google.com/s2/favicons?domain=adobe.com&sz=128",
+      name: "Adobe",
+      datePosted: "6 days ago",
+      post: "Graphic Designer",
+      tag1: "Part-Time",
+      tag2: "Junior Level",
+      pay: "$90/hr",
+      location: "Islamabad, Pakistan",
+    },
+    {
+      brandLogo: "https://www.google.com/s2/favicons?domain=ibm.com&sz=128",
+      name: "IBM",
+      datePosted: "2 weeks ago",
+      post: "Data Analyst",
+      tag1: "Full-Time",
+      tag2: "Mid Level",
+      pay: "$105/hr",
+      location: "Islamabad, Pakistan",
+    },
+    {
+      brandLogo: "https://www.google.com/s2/favicons?domain=oracle.com&sz=128",
+      name: "Oracle",
+      datePosted: "3 days ago",
+      post: "Cloud Engineer",
+      tag1: "Full-Time",
+      tag2: "Senior Level",
+      pay: "$135/hr",
+      location: "Islamabad, Pakistan",
+    },
+  ];
 
   return (
-    <div className="parent">
-      {jobs.map(function(elem,idx){
-        return <div key={idx}>
-          <Card
-          brandLogo={elem.brandLogo}
-          name={elem.name}
-          datePosted={elem.datePosted}
-          post={elem.post}
-          tag1={elem.tag1}
-          tag2={elem.tag2}
-          pay={elem.pay}
-          location={elem.location}
-        />
-        </div>
-      })}
-    </div>
-  )
-}
+    <main className="jobs-container">
+      {jobs.map((job) => (
+        <Card key={job.name} {...job} />
+      ))}
+    </main>
+  );
+};
 
-export default App
+export default App;
--- cards-project/src/components/Card.jsx
+++ cards-project/src/components/Card.jsx
@@ -1,45 +1,56 @@
-import React from 'react'
-import { Bookmark } from 'lucide-react'
-const Card = (props) => {
+import React from "react";
+import { Bookmark } from "lucide-react";
+
+const Card = ({
+  brandLogo,
+  name,
+  datePosted,
+  post,
+  tag1,
+  tag2,
+  pay,
+  location,
+}) => {
   return (
-    <div>
-      <div className="parent">
-      <div className="card">
+    <article className="card">
+      {/* Top */}
+      <div className="top">
+        <img src={brandLogo} alt={`${name} logo`} />
 
-        <div className="top">
-          <img
-            src={props.brandLogo}
-            alt="icon"
-          />
-
-          <button>
-            SAVE <Bookmark size={10} />
-          </button>
-        </div>
-
-        <div className="center">
-          <h3>{props.name} <span>{props.datePosted}</span></h3>
-          <h2>{props.post}</h2>
-          <div className="tag">
-           <p>{props.tag1}</p>
-             <p>{props.tag2}</p>
-          </div>
-        </div>
-
-        <div className="bottom">
-          <div>
-              <h3>{props.pay}</h3>
-              <p>{props.location}</p>
-          </div>
-          <button>Apply Now</button>
-        
-        </div>
-
+        <button className="save-btn" type="button">
+          SAVE
+          <Bookmark size={11} />
+        </button>
       </div>
-      
-    </div>
-    </div>
-  )
-}
 
-export default Card
+      {/* Center */}
+      <div className="center">
+        <h3>
+          {name}
+          <span>{datePosted}</span>
+        </h3>
+
+        <h2>{post}</h2>
+
+        <div className="tags">
+          <p>{tag1}</p>
+          <p>{tag2}</p>
+        </div>
+      </div>
+
+      {/* Bottom */}
+      <div className="bottom">
+        <div className="job-info">
+          <h3>{pay}</h3>
+          <p>{location}</p>
+        </div>
+
+        <button className="apply-btn" type="button">
+          Apply Now
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default Card;

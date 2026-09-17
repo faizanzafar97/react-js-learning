55eced0bd17cb08fbe274762b1ff0796d6c3fe1c
faizanzafar97 <faizanzafar8672@gmail.com>
2026-09-17 16:36:52 +0500

    understanding to module css

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './compnents/header/header.jsx'
+import Button from './compnents/button/button.jsx'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Button />
+    </>
+  )
+}
+
+export default App
